82eb862506d5f46ad9b737cd0f44526ea3cd3f91
christphrd <diep.christopher@gmail.com>
2018-09-02 21:51:08 -0400

    routes to add in todo

--- routes/todos.js
+++ routes/todos.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/index');
 
+//GET todo list
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
+
+//GET todo
+
+//POST new todo
+
+//UPDATE todo
+
+//DELETE todo
 
 module.exports = router;
