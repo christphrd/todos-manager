e41651c34d1f54a93209c11f15988e8d752ed837
christphrd <diep.christopher@gmail.com>
2018-09-02 21:53:21 -0400

    now with routes in todo. CRUD

--- routes/todos.js
+++ routes/todos.js
@@ -8,11 +8,23 @@ router.get('/', function (req, res, next) {
 });
 
 //GET todo
+router.get('/:id', function (req, res, next) {
+
+});
 
 //POST new todo
+router.post('/', function (req, res, next) {
+
+});
 
 //UPDATE todo
+router.put('/:id', function (req, res, next) {
+
+});
 
 //DELETE todo
+router.delete('/:id', function (req, res, next) {
+
+});
 
 module.exports = router;
